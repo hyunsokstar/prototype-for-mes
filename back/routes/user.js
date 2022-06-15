a600d87f31aea896391494ca4b070f81048edeb6
ohyunsok <hyunsokstar@gmail.com>
2022-06-15 18:21:31 +0900

    update for log

--- back/routes/user.js
+++ back/routes/user.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 
-
 // router.get('/', (req, res) => {
 //     res.send("/post/ test");
 // });
 
 router.patch("/login", async (req, res, next) => {
     console.log("login !!");
+    console.log("req.body.username : ", req.body.username);
+    console.log("req.body.password : ", req.body.password);
 
     res.status(200).json({ message: '로그인 성공' })
 });
