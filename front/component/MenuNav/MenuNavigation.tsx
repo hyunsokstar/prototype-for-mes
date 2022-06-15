502c2aa4cada4b0891bafaecdbba019eb9f08a5d
ohyunsok <hyunsokstar@gmail.com>
2022-06-15 10:52:34 +0900

    update for menu

--- front/component/MenuNav/MenuNavigation.tsx
+++ front/component/MenuNav/MenuNavigation.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { MenuNavComponent, MenuNavItem, MenuText, SideMenuItem } from '../../styles/styledComponents'
 
 export const POINT_COLOR: string = '#19B9DF' as const
@@ -15,26 +15,38 @@ interface IProps {
 function MenuNavigation({ pageType, subType }: IProps) {
     const [menuType, setMenuType] = useState<IMenu>(pageType ?? "")
 
+    const changeMenuType = (selectType: IMenu) => {
+        setMenuType(selectType)
+
+    }
 
     return (
         <div style={{ display: 'flex' }}>
             <MenuNavComponent >
                 <div>
-                    <MenuNavItem style={{backgroundColor: menuType === "HOME" ? "skyblue" : undefined}} >
+                    <MenuNavItem style={{ backgroundColor: menuType === "HOME" ? "skyblue" : undefined }} onClick={() => {
+                        changeMenuType("HOME")
+                    }}>
                         <MenuText>HOME</MenuText>
                     </MenuNavItem>
-                    <MenuNavItem >
+                    <MenuNavItem style={{ backgroundColor: menuType === "BASIC" ? "skyblue" : undefined }} onClick={() => {
+                        changeMenuType("BASIC")
+                    }}>
                         <MenuText>기준정보관리</MenuText>
                     </MenuNavItem>
-                    <MenuNavItem >
+                    <MenuNavItem style={{ backgroundColor: menuType === "MES" ? "skyblue" : undefined }} onClick={() => {
+                        changeMenuType("MES")
+                    }}>
                         <MenuText>MES</MenuText>
                     </MenuNavItem>
-                    <MenuNavItem >
+                    <MenuNavItem style={{ backgroundColor: menuType === "PMS" ? "skyblue" : undefined }} onClick={() => {
+                        changeMenuType("PMS")
+                    }}>
                         <MenuText>PMS</MenuText>
                     </MenuNavItem>
                 </div>
-            </MenuNavComponent>
-        </div>
+            </MenuNavComponent >
+        </div >
     )
 }
 
